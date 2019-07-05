54a3e7e9f998a175f0fb1e8a7d582b5700f58cf4
IrynaFomina <onlymyy@gmail.com>
2019-07-05 09:12:15 +0300

    hw25

--- src/main/java/org/demo/web/MainController.java
+++ src/main/java/org/demo/web/MainController.java
@@ -1,5 +1,7 @@
 package main.java.org.demo.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,15 +11,16 @@ import java.util.List;
 @Controller
 @RequestMapping("main")
 public class MainController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     public MainController() {
-        System.out.println("Controller created");
+        LOGGER.info("Controller created");
     }
 
     @RequestMapping("get-some-data")
     @ResponseBody
     public String[] getSomeData() {
-        System.out.println("Controller method invoked");
+        LOGGER.info("Controller method invoked");
         return new String[]{"1111", "2222", "333"};
     }
 }
